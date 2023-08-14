762d32b54ae0035a7b5702854a6ab6bb35049310
Mauricio <mauricio.tp_@outlook.com>
2023-08-14 15:08:30 -0300

    feat: create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
     </div>
   );
 }
