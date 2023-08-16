f11abd0840e1d17b0d85ab9fb3c48bd0110cbec6
Mauricio <mauricio.tp_@outlook.com>
2023-08-16 12:27:42 -0300

    feat: render post dinamically

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,45 @@ import './global.css';
 import styles from './App.module.css';
 import { Sidebar } from './components/Sidebar';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/mauriciotp.png',
+      name: 'Maurício Pinto',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-08-16 11:51:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/carnotbraun.png',
+      name: 'Carnot Braun',
+      role: 'Data Scientist',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-08-16 07:51:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -11,7 +50,13 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map((post) => (
+            <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
